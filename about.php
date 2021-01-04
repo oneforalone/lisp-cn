0996781465e13961ed16cbba7b2fd2f15dd9deee
yuqi <yuqi.lyle@outlook.com>
2021-01-04 15:37:23 +0800

    first add about page

---
+++ about.php
@@ -0,0 +1,28 @@
+<html lang="zh-cn">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>关于</title>
+
+    <?php include 'header.html'; ?>
+  </head>
+
+  <body>
+  <?php include 'nav.html'; ?>
+  <main class="main">
+    <h2>
+      暂时还没编好。纯爱发电，求个赞助：）
+    </h2>
+    <div class="book-cover">
+
+        <img alt="alipay QR" src="assets/alipay.jpg"
+             style="width: 200px; height: 200px;">
+
+        <img alt="wechat pay QR" src="assets/wechat_pay.jpg"
+             style="width: 200px; height: 200px;">
+
+    </div>
+  </main>
+  </body>
+
+</html>
